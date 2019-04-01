4947cc3314eb19c36c7d51bceec30d4cc89764e0
nivleM_ <46630582+nivleM-ed@users.noreply.github.com>
2019-04-01 16:16:43 +0800

    Update printName.php
    Added class methods for printName.php

--- Lab_6/printName.php
+++ Lab_6/printName.php
@@ -1,5 +1,5 @@
 <body>
-    <form  method="POST">
+    <form method="POST">
         <label>Name: </label><input type="text" name="name">
         <label>Times: </label><input type="number" name="num">
         <input type="submit" name="submit" value="Print">
@@ -7,21 +7,39 @@
 </body>
 
 <?php
-function printName($name, $num) {
-    for($x=0; $x<$num; $x++) {
-        echo $name;
-        echo "<br>";
+
+class person {
+    var $name;
+
+    function __construct($name) {
+        $this->name = $name;
+    }
+
+    function set_name($name) {
+        $this->name = $name;
+    }
+    function get_name() {
+        return $this->name;
+    }
+
+    function printName($num) {
+        for ($x = 0; $x < $num; $x++) {
+            echo $this->name;
+            echo "<br>";
+        }
     }
 }
 
-if (isset($_POST["submit"])) {
-    $name = $_POST["name"];
-    $num = $_POST["num"];
 
-    printName($name, $num);
-}   
-?> 
+if (isset($_POST["submit"])) {
+    $num = $_POST["num"];
+    $name = $_POST["name"];
+
+    $new_person = new person($name);
+    $new_person->printName($num);
+}
+?>
 
 <script>
-    
-</script>
+
+</script> 
